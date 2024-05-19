993f107a19fbfe3af752d41d73571d23ff88f193
Essaadia erraiss <128802580+saadiaerr@users.noreply.github.com>
2024-05-19 13:58:17 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-"""
-User Module
-"""
 
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+"""define User class mod"""
+
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """
     User class
 
     Attributes:
-        __tablename__ (str): The name of the MySQL table to store Users
-        email (sqlalchemy String): The user's email address
-        password (sqlalchemy String): The user's password
-        first_name (sqlalchemy String): The user's first name
-        last_name (sqlalchemy String): The user's last name
-        places (sqlalchemy relationship): The User-Place relationship
-        reviews (sqlalchemy relationship): The User-Review relationship
+        email (str): users email address
+        password (str): users password
+        first_name (str): user first name
+        last_name (str): user last name
 
     Methods:
-        __init__(self, *args, **kwargs): Initializes a User instance
+        __init__(*args, **kwargs): initializes a User instance
     """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a User instance"""
+        super().__init__(*args, **kwargs)
